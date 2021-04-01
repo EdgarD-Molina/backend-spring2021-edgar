c00eae6ead92bd8b6e936366474c0184cfde1865
Danny <dannyplaysthebass@gmail.com>
2021-04-01 11:34:12 -0700

    Daily push after class

--- week2/filereader.js
+++ week2/filereader.js
@@ -7,20 +7,23 @@ let contents = arguments[4];
 let fileName2 = contents;
 let mergedFile = arguments[5]
 
-if (fileName === undefined) {
+
+// Reassign contents variable as an empty array element holds the value undefined.
+if (action === undefined) {
     console.log(`Welcome to file reader. Please provide the file name at the end of the command.
     
     Example:                  
     Read an existing file     node filereader.js read myfile.txt
-    Write a new file:         node filereader.js write newFile.txt
-    Update an existing file:  node filereader.js update myFile.txt
-    Merge any current files:  node filereader.js merge file1 file 2
-    Delete an existing file:  node filereader.js delete myFile.txt`)
+    Write a new file:         node filereader.js write newFile.txt "New text to write"
+    Update an existing file:  node filereader.js update myFile.txt "Text you want to add"
+    Merge any current files:  node filereader.js merge file1.txt file2.txt mergedfilename.txt  
+    Delete an existing file:  node filereader.js delete myFile.txt true `)
     return;
 }
 
 if (action === "read") {
     
+    //Checks if file exists
     if (fs.existsSync(fileName)) {
         //Read the file based on the arguent in the command.
         let fileContents = fs.readFileSync(fileName, "utf-8");
@@ -32,12 +35,14 @@ if (action === "read") {
             }
 
 } else if (action === "write") {
+    //Check if file exists
     if (fs.existsSync(fileName)) {
         console.log("A file with this name already exists. Choose different name.");
-    }
+    } else {
+        //If it doesn't exist, we will make it safely.
     fs.writeFileSync(fileName, contents, "utf-8");
     console.log("New file created as " + fileName, ".");
-
+    }
 } else if (action === "update") {
 
     if(fs.existsSync(fileName)) {
@@ -51,6 +56,16 @@ if (action === "read") {
             }
     
 } else if (action === "delete") {
+    if(fs.existsSync(fileName)) {
+        if(contents === "true") {
+            fs.unlinkSync(fileName);
+            console.log(fileName + " succesfully deleted.");
+        } else {
+            console.log("Sure you want to delete? Run command with 'true' as final argument");
+        }  
+    } else {
+        console.log("No file with that name exists. Check argument.");
+     }
 
 } else if (action === "merge") {
     if (fs.existsSync(fileName) && fs.existsSync(fileName2)) {
@@ -70,4 +85,8 @@ if (action === "read") {
         console.log("Sorry, one of these files does not exist. Please check spelling or directory.");
     }
 
+} else if (action === "copy") {
+
+} else {
+    console.log("There is no action by that name. You can read, write, update, delete, merge, or copy.");
 }
---
+++ week2/todo-cli/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+
+class Task {
+    constructor(text, priority, dueDate) {
+        this.text = text;
+        this.dueDate = dueDate;
+        this.dateCreated = new Date();
+        this.priority =priority;
+        this.dateCompleted = null;
+
+    }
+}
+
+
+
+let taskArray = [];
+
+let action = process.argv[2];
+let text = process.argv[3];
+let priority = process.argv[4];
+
+if (action === "add") {
+       taskArray.push(new Task(text,priority));
+        console.log("Task added");
+}
+
+fs.writeFileSync("tasks.txt", String(taskArray), "utf-8");
+
+console.log(taskArray);
