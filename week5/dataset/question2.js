98f4538ca75cea217eb769d5939850a417b2d40e
Danny <dannyplaysthebass@gmail.com>
2021-05-11 12:03:42 -0700

    Finished updating notes & comments on dataset. Added new app with react

--- week5/dataset/question2.js
+++ week5/dataset/question2.js
@@ -1,11 +1,14 @@
+// Loads in the array with all entry objects.
 const dataset = require("./dataset.js");
 let objectArray = dataset.objectArray;
 
 
 // QUESTION 2: What day of the week is (on average) the most active? What hour (on average) for the week is the most active?
+// Incident Time and Incident Day of Week.
 
 console.log("What day of the week is (on average) the most active? What hour (on average) for the week is the most active?");
-// Incident Time and Incident Day of Week.
+
+// Object to hold the count for each weekday.
 
 let dayCounter = {
     "Sunday": 0,
@@ -17,18 +20,26 @@ let dayCounter = {
     "Saturday": 0
 };
 
+// Array to hold the count for each hour of a 24-hour period. The index will represent the individual hours.
 let hourCounter = [];
 
+//Go through each entry object.
 objectArray.forEach(function (entry) {
 
+    //Gran the hour value of the Incident Time property.
     let hour = parseInt(entry['Incident Time'].split(":")[0]);
 
+    //If the house does not exist...
     if (hourCounter[hour] === undefined) {
+        //...we create it, and assign 1.
         hourCounter[hour] = 1;
+        //Otherwise add a 1 to the count for that hour.
     } else {
         hourCounter[hour]++;
     }
 
+    //Get the value of the day of the week for the current entry object...
+    //...and match to a case to add a count to that day in our day count object.
     switch (entry['Incident Day of Week']) {
         case "Sunday":
             dayCounter.Sunday++;
@@ -54,21 +65,30 @@ objectArray.forEach(function (entry) {
 
 });
 
+//Hold the highest number for the most common day.
 let highestCount = 0;
 let highestDay;
 
+///Go through the day counter and find the largest number.
  for (let day in dayCounter) {
+
+    //if a number is larfer than the current largest...
     if (dayCounter[day] > highestCount) {
+        //replace the current largest number with the new day.
         highestCount = dayCounter[day];
         highestDay = day;
     }
 };
 
+//Display results.
 console.log("The day with the most activity is " + highestDay);
 
+//Find the highest count value and get that value.
 let highestHour = Math.max(...hourCounter);
+// Using the highest value, find the index of that highest value.
 let highestIndex = hourCounter.indexOf(highestHour);
 
+// Display results, based on AM or PM.
 if (highestIndex >= 12) {
     console.log("The most active hour of the day is " + (highestIndex === 12 ? "12": highestIndex - 12) + "PM.");
 } else {
--- week5/dataset/question3.js
+++ week5/dataset/question3.js
@@ -1,25 +1,34 @@
+// Loads in the array with all entry objects.
 const dataset = require("./dataset.js");
 let objectArray = dataset.objectArray;
 
 // QUESTION 3: Which neighborhood is the most active and least active? Supervisor district? (with and without supplements)
 console.log("Which neighborhood is the most active and least active? Supervisor district?");
+
+// Create objects to hold our results fo rniehgborhood and district.
 let resultsQuestion3 = {};
 let resultsQuestion3District = {};
 
+//Go through each entry object..
 for (let i = 0; i < objectArray.length; i++) {
 
+    //...store each entry object into the variable entry.
     let entry = objectArray[i];
 
+    // check if the results object has that neighborhood.
     if (resultsQuestion3.hasOwnProperty(entry['Analysis Neighborhood'])) {
 
+        // ifit does, add a 1 to the value of that neighborhood property.
         var neighborhood = entry['Analysis Neighborhood'];
 
         resultsQuestion3[neighborhood] += 1;
     } else {
+        // if it doesn't, assign it a 1.
         resultsQuestion3[entry['Analysis Neighborhood']] = 1;
     }
 
 
+    //Just as the biehgborhodo code above, do the same for district.
     if (resultsQuestion3District.hasOwnProperty(entry['Supervisor District'])) {
         resultsQuestion3District[entry['Supervisor District']] += 1;
     } else {
@@ -27,28 +36,37 @@ for (let i = 0; i < objectArray.length; i++) {
     }
 }
 
+// Track the highest niehgborhood.
 let highestNeighborhoodCount = 0;
 let highestNeighborhoodName;
 
+// For each neighborhood...
 for (let neighborhood in resultsQuestion3) {
 
+        // ... Check if the current neighborhood has a higher count than then highest count....
     if (resultsQuestion3[neighborhood] > highestNeighborhoodCount) {
+        // If it does, replace the highest count with the current neighborhood count.
         highestNeighborhoodCount = resultsQuestion3[neighborhood];
         highestNeighborhoodName = neighborhood;
     }
 }
 
+// Track the highest values for district.
 let highestDistrict = 0;
 let highestDistrictName;
 
+// For each district...
 for (let district in resultsQuestion3District) {
 
+    //Check if the current ditrict has a higher count thatn the current highest count...
     if (resultsQuestion3District[district] > highestDistrict) {
+        //If it does, then replace the current highest count with the current neighborhood count.
         highestDistrict = resultsQuestion3District[district];
         highestDistrictName = district;
     }
 }
 
+//Display results.
 console.log(`The neighborhood with the highest activity is ${highestNeighborhoodName} with ${highestNeighborhoodCount} entries.`);
 console.log(`The district with the highest activity is ${highestDistrictName} with ${highestDistrict} entries.`);
 
